36ed6e133c916bdc0a401a1d2266e3cc939e2ecf
hemanthindala <hemanthindala2001@gmail.com>
2024-03-01 01:31:34 -0500

    new9

--- CentralBank/settings.py
+++ CentralBank/settings.py
@@ -39,7 +39,12 @@ INSTALLED_APPS = [
     'accounts',
     'profiles',
     'admins',
-    'corsheaders'
+    'corsheaders',
+    'CentralBank',
+    'templates',
+    'templates',
+    'static',
+    'media'
 ]
 
 MIDDLEWARE = [
